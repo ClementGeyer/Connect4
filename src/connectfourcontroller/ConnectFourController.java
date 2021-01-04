365645b6dd96a41f748beeba02d6fc36ba4d97ad
EDEL GAUTIER <gautier.edel@etu.unistra.fr>
2021-01-04 21:53:45 +0100

    Add Observer pattern + play() method for Controller

--- src/connectfourcontroller/ConnectFourController.java
+++ src/connectfourcontroller/ConnectFourController.java
@@ -1,7 +1,6 @@
 package connectfourcontroller;
 
 import connectfourmodel.Game;
-import connectfourmodel.Piece;
 
 public class ConnectFourController extends TemplateController
 {
@@ -10,16 +9,31 @@ public class ConnectFourController extends TemplateController
         setGame(g);
     }
 
-
-
     @Override
     public void play()
     {
+        // main: setCurrentPlayer(getGame().getPlayer(0));
 
+        getCurrentPlayer().play();
+        insertPiece();
+
+        // playCoin dans vue va setCarteCourante & appeler play()
+
+        if(getGame().winner() == null || getCurrentPlayer().getPieces().size() > 0)
+        {
+            setCurrentPlayer( getCurrentPlayer() == getGame().getPlayer(0) ? getGame().getPlayer(1) : getGame().getPlayer(0));
+        }
+        else
+        {
+            setGameEnded(true);
+        }
+
+        notifyObservers();
     }
 
     @Override
-    public void insertPiece(Piece p, int column) {
-        getGame().insertPiece(p, column);
+    public void insertPiece()
+    {
+        getGame().insertPiece(getCurrentPlayer().getPieces().get(0), getCurrentColumn());
     }
 }
